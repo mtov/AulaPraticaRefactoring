c5d049cc61541498cbd178d9774f6c950fde36c4
Rodolfo de <>
2022-11-29 18:30:11 +0000

    Versão inicial do sistema. Implementação das classes Customer, Movie e Rental

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+* Engenharia de Software Moderna 
+* Prof. Marco Tulio Valente
+* 
+* Roteiro Prático sobre refactoring
+* 
+* Classes iniciais do sistema de videolocadora
+*
+*/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Apenas um exemplo de refatoração...");
+  }
+}
